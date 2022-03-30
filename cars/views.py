f4f2c426bd645375103d5445b209e72c69787e0a
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-03-30 14:52:34 -0400

    Added project urls, created first view function, created cars app urls file

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view  #Decorator assigns certain permissions to each function, define which HTTP request types each function is capable of handling
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def cars_list(request):
+
+    return Response('ok!')
