6be9c5a2eea288853e1c91c9e9f255fd3a98470a
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-03-30 15:58:31 -0400

    Created new view function for getting cars by id + Added new urls path for car_detail view function

--- cars/urls.py
+++ cars/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cars_list),
+    path('<int:pk>', views.car_detail)  #<int:pk> in route indicates that the value passed into the path will be used as the pk argument in car_detail(request, pk) and must be an integer
 ]
